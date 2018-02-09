e767784152593b31b794d496e8d2f59cfeb0bb89
Leon Daros <leon@kincode.com.br>
2018-02-09 14:56:13 -0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+
+  state = {
+    value: 0,
+  };
+
+  handleIncrement = () => {
+    const newValue = this.state.value + 1
+    this.setState({ value: newValue });
+  }
+
+  handleDecrement = () => {
+    const newValue = this.state.value - 1
+    this.setState({ value: newValue });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          <label className="label">{this.state.value}</label>
+        </h1>
+        <div className="buttons">
+          <button type="button" className="btn btn-primary btn-lg" onClick={this.handleIncrement}>Somar</button>
+          <button type="button" className="btn btn-primary btn-lg" onClick={this.handleDecrement}>Subtrair</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
